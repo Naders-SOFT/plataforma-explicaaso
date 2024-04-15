05d350f6f209e033d424e8ccdb577e083c39dfab
LeonardoIshida <leoishida@hotmail.com>
2024-04-15 20:41:48 -0300

    seila

---
+++ client/explicaaso/src/components/ContainerMateria/index.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const Materias = styled.div`
+    display: flex;
+`
---
+++ client/explicaaso/src/components/PaginaAluno/index.js
@@ -0,0 +1,12 @@
+import Header from '../Header'
+import styled from 'styled-components';
+
+function PaginaAluno(props) {
+    return (
+        <Header>
+            
+        </Header>
+    );
+}
+
+export default PaginaAluno;
