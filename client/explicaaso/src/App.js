1516d0196d9c34e1c81f1dfc53f8913f7efc0be8
RafaelCGConrado <106274045+RafaelCGConrado@users.noreply.github.com>
2024-04-19 19:36:09 -0300

    Update App.js

--- client/explicaaso/src/App.js
+++ client/explicaaso/src/App.js
@@ -22,12 +22,10 @@ function App() {
     // inteira, nele temos apenas o componente Header, por enquanto. 
     // Note que a estilização desse container é feita através 
     // de styled-components algumas linhas acima.
-    // <AppContainer>
-    //   <Header/>
-    //   <BarrinhaTurquesa/>
-      
-    // </AppContainer>
-    <PaginaNoticias/>
+    <AppContainer>
+      <Header/>
+      <BarrinhaTurquesa/>
+    </AppContainer>
   );
 }
 
