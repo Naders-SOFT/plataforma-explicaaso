20970a8849a3fa5bb12c30e888071e4905a06657
Victor Hugo Amaral <amaral.vht@gmail.com>
2024-04-29 11:59:22 -0300

    Colocadas as fontes da página inicial.

--- client/explicaaso/src/App.js
+++ client/explicaaso/src/App.js
@@ -2,12 +2,31 @@ import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import Header from './components/Head/Header';
 import Footer from './components/FooterComponents/Footer';
+import { createGlobalStyle } from 'styled-components';
+
 
 // ------- PÁGINA PRINCIPAL (LANDING PAGE) --------
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Segoe UI';
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+
+  li {
+    list-style: none;
+  }
+`
+
 // ESTILIZAÇÃO DO COMPONENTE
 const AppContainer = styled.div`
-  
 `
 
 
@@ -35,6 +54,7 @@ function App() {
     // Note que a estilização desse container é feita através 
     // de styled-components algumas linhas acima.
     <AppContainer>
+      <GlobalStyle/>
       <Header isMobile={isMobile}/>
       <Footer isMobile={isMobile}/>
     </AppContainer>    
