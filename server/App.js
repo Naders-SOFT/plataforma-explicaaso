06b564d5b69149d84cbfe468155a025c2f8ac08c
LeonardoIshida <leoishida@hotmail.com>
2024-05-26 15:44:38 -0300

    salvando antes do npm init

--- server/App.js
+++ server/App.js
@@ -2,15 +2,20 @@ import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3'
 import express from 'express'
 import multerS3 from 'multer-s3'
 import routes from './routes/api.routes.js'
+import path from 'path'
 
 const app = express()
 
 app.use(express.static('public'))
-app.use(routes)
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }))
+app.use('/api', routes)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(3003,  () => {
     console.log('Servidor ouvindo na porta 3003.') 
--- server/routes/api.routes.js
+++ server/routes/api.routes.js
@@ -1,18 +1,9 @@
 import express from 'express'
 import multer from 'multer'
-import pdfModels from '../models/pdf.models.js'
+import pdfModels from '../controllers/pdf.controller.js'
 
 const routes = express.Router()
 
-routes.post('/pdf', pdfModels.upload.single('pdf'), async (request, response) => {
-    try {
-      const fileUrl = `http://127.0.0.1:9000/teste/${request.file.key}`;
-      response.json({ message: 'Arquivo enviado com sucesso!', url: fileUrl });
-    } catch (error) {
-      console.error(error);
-      response.status(500).json({ message: 'Erro ao enviar o arquivo' });
-    }
-  }
-);
+routes.post('/pdf', pdfModels.uploadArquivo);
 
 export default routes
