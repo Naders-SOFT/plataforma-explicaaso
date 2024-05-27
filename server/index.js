ab3f74b75d89e4fe492c63ff8fd950ad2c0b9257
Victor Hugo Amaral <amaral.vht@gmail.com>
2024-05-27 14:28:41 -0300

    Instalado firebase no client

--- server/index.js
+++ server/index.js
@@ -4,14 +4,24 @@ import path from 'path'
 import cors from 'cors'
 import pdfController from './controllers/pdf.controller.js'
 
+// Cria uma aplicação express:
 const app = express()
 
 app.use(express.static('public'))
+
+// Habilita a aplicação a trabalhar com JSONs:
 app.use(express.json())
+
+
 // app.use('/api', routes)
 app.use(cors({
     origin: 'http://localhost:3000'  // Replace with your frontend's origin
 }));
+
+// PARTE DOS USUÁRIOS:
+
+
+// PARTE DOS PDFs:
 
 app.post('/pdf', pdfController.upload.single('file'), function(request, response, err) {
     if (err) {
