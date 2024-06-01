1d99684ab6b8407ca313558bf11ea67d56c134f2
João P. Hamata <128185132+joaophamata@users.noreply.github.com>
2024-05-31 21:58:35 -0300

    Update index.js

--- client/explicaaso/src/components/Disciplina/Avisos/index.js
+++ client/explicaaso/src/components/Disciplina/Avisos/index.js
@@ -4,6 +4,30 @@ import TituloDisciplina from '../Titulo';
 import ItemAdicionar from '../ItemAdicionar';
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import Frentes from '../Frentes/index';
+import gramatica from '../../../images/frentes/gra.jpeg';
+import literatura from '../../../images/frentes/lit.jpeg';
+import interpretacao from '../../../images/frentes/int.jpeg';
+import ingles from '../../../images/frentes/ing.jpeg';
+import redacao from '../../../images/frentes/red.jpeg';
+import his1 from '../../../images/frentes/his1.jpeg';
+import his2 from '../../../images/frentes/his2.jpeg';
+import geo1 from '../../../images/frentes/geo1.jpeg';
+import geo2 from '../../../images/frentes/geo2.jpeg';
+import fil from '../../../images/frentes/fil.jpeg';
+import soc from '../../../images/frentes/soc.jpeg';
+import bio1 from '../../../images/frentes/bio1.jpeg';
+import bio2 from '../../../images/frentes/bio2.jpeg';
+import bio3 from '../../../images/frentes/bio3.jpeg';
+import fis1 from '../../../images/frentes/fis1.jpeg';
+import fis2 from '../../../images/frentes/fis2.jpeg';
+import fis3 from '../../../images/frentes/fis3.jpeg';
+import qui1 from '../../../images/frentes/qui1.jpeg';
+import qui2 from '../../../images/frentes/qui2.jpeg';
+import qui3 from '../../../images/frentes/qui3.jpeg';
+import mat1 from '../../../images/frentes/mat1.jpeg';
+import mat2 from '../../../images/frentes/mat2.jpeg';
+import mat3 from '../../../images/frentes/mat3.jpeg';
 
 const AvisosPainel = styled.ul`
     display: flex;
@@ -34,8 +58,34 @@ function Avisos(props) {
     }, [])
 
     const listaPdfs = pdfs.map(pdf => (
-        <ItemAviso tituloAviso={pdf.titulo} link={pdf.link}></ItemAviso>
+        <ItemAviso key={pdf.id} tituloAviso={pdf.titulo} link={pdf.link}></ItemAviso>
     ))
+
+    const frentes = [
+        { name: 'gramatica', image: gramatica },
+        { name: 'literatura', image: literatura },
+        { name: 'interpretacao', image: interpretacao },
+        { name: 'ingles', image: ingles },
+        { name: 'redacao', image: redacao },
+        { name: 'his1', image: his1 },
+        { name: 'his2', image: his2 },
+        { name: 'geo1', image: geo1 },
+        { name: 'geo2', image: geo2 },
+        { name: 'fil', image: fil },
+        { name: 'soc', image: soc },
+        { name: 'bio1', image: bio1 },
+        { name: 'bio2', image: bio2 },
+        { name: 'bio3', image: bio3 },
+        { name: 'fis1', image: fis1 },
+        { name: 'fis2', image: fis2 },
+        { name: 'fis3', image: fis3 },
+        { name: 'qui1', image: qui1 },
+        { name: 'qui2', image: qui2 },
+        { name: 'qui3', image: qui3 },
+        { name: 'mat1', image: mat1 },
+        { name: 'mat2', image: mat2 },
+        { name: 'mat3', image: mat3 },
+      ];
 
     return (
         <Container>
@@ -45,6 +95,7 @@ function Avisos(props) {
                     <ItemAdicionar/>
                     <TituloDisciplina tituloDisciplina={props.tituloDisciplina}/>
                     {listaPdfs}
+                    <Frentes frentes={frentes} />
                 </AvisosPainel>
             }
             {
@@ -53,6 +104,7 @@ function Avisos(props) {
                     <TituloDisciplina tituloDisciplina={props.tituloDisciplina}/>
                     <ItemAdicionar/>
                     {listaPdfs}
+                    <Frentes frentes={frentes} />
                 </AvisosPainel>
             }
         </Container>
