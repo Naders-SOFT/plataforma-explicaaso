8d8ec441158c73ef061d06a4431a2e693bdf94aa
João P. Hamata <128185132+joaophamata@users.noreply.github.com>
2024-06-05 11:08:09 -0300

    Update index.js

--- client/explicaaso/src/components/Aluno/Materias/index.js
+++ client/explicaaso/src/components/Aluno/Materias/index.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import CardMateria from '../CardMateria';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Frentes from '../../Disciplina/Frentes';
-import PaginaBlog from '../../../pages/PaginaBlog';
 
 const MOBLMATERIAS = styled.ul`
     display: flex;
@@ -32,28 +31,12 @@ const Container = styled.div`
 
 function ContainerMateria(props) {
     const materias = props.materias?.map((mat) => (
-        // const linha = "/pagina-aluno/" + {mat.materia} + "/frentes";
-        <div>
-            {/* <Link to="/pagina-aluno/Materias/frentes" > */}
-                <CardMateria imgSrc={mat.img} materia={mat.materia}/>
-            {/* </Link> */}
-            {/* <Routes>
-                <Route path="/pagina-aluno/Materias/frentes" element={<Frentes frentes={props.frentes}/>} />
-            </Routes> */}
+        <div key={mat.materia}> 
+            <Link to='/pagina-aluno/Materias/frentes'>
+                <CardMateria imgSrc={mat.img} materia={mat.materia} />
+            </Link>
         </div>
     ))
-
-    const card = (
-        <div>
-        <Link to="/pagina-aluno/Materias/frentes" >
-            <CardMateria materia="test"/>
-        </Link>
-        <Routes>
-                <Route path="/jijij" element={<PaginaBlog />} />
-        </Routes>
-        </div>
-    )
-    
 
     return (
         <Container>
@@ -65,7 +48,6 @@ function ContainerMateria(props) {
             {!props.isMobile &&
                 <DSKMATERIAS>
                     {materias}
-                    {card}
                 </DSKMATERIAS>
             }
         </Container>
