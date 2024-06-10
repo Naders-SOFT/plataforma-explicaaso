2fb7b09e525b69e61e4e65910c881b718114945c
LeonardoIshida <leoishida@hotmail.com>
2024-06-10 12:33:56 -0300

    update crud do pdf, listando pdfs por frentes incluido

--- server/routes/pdf.routes.js
+++ server/routes/pdf.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import multer from 'multer';
-import { createPdf, listPdfs, deletePdf } from '../controllers/pdf.controller.js'; 
+import { createPdf, listPdfs, deletePdf, listPdfsFrente } from '../controllers/pdf.controller.js'; 
 
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 router.get('/list', listPdfs);
+
+router.get('/list/:frente', listPdfsFrente);
 
 router.post('/posts', upload.single('arq-pdf'), createPdf)
 
