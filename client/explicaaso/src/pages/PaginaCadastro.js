242789d3088d88122ee96fa04878e39bd2c9a115
João P. Hamata <128185132+joaophamata@users.noreply.github.com>
2024-06-14 20:29:02 -0300

    Adição de feedbacks para senhas incompatíveis, sucesso, falha, além de mecanismo de sugestão de senha

--- client/explicaaso/src/pages/PaginaCadastro.js
+++ client/explicaaso/src/pages/PaginaCadastro.js
@@ -1,10 +1,10 @@
-import styled, { css } from "styled-components"
-import React, { useState } from 'react'
-import axios from "axios"
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import axios from 'axios';
 
 const ContainerPag = styled.div`
-    width: 100%;
-`
+  width: 100%;
+`;
 
 const FormContainer = styled.div`
   display: flex;
@@ -16,10 +16,9 @@ const FormContainer = styled.div`
 `;
 
 const CadastroTitulo = styled.h1`
-  color: #FFCC00;
+  color: #ffcc00;
   font-size: 36px;
-`
-
+`;
 
 const Formulario = styled.form`
   background-color: #003466;
@@ -42,7 +41,7 @@ const inputStyles = css`
   border: 1px solid #ddd;
   border: none;
   border-radius: 4px;
-  color: white; 
+  color: white;
   margin-bottom: 20px;
   font-size: 16px;
   background-color: #265a9b;
@@ -59,7 +58,7 @@ const selectStyles = css`
   border: 1px solid #ddd;
   border: none;
   border-radius: 4px;
-  color: white; 
+  color: white;
   margin-bottom: 20px;
   font-size: 16px;
   background-color: #265a9b;
@@ -69,7 +68,6 @@ const selectStyles = css`
     box-shadow: 0 0 0 3px rgba(0, 123, 255, 0.2);
   }
 `;
-
 
 const Label = styled.label`
   color: white;
@@ -89,75 +87,94 @@ const Botao = styled.button`
   width: 30%;
   border: none;
   border-radius: 8px;
-  background-color: #FFCC00;
+  background-color: #ffcc00;
   font-size: 20px;
   font-weight: 600;
   color: #003466;
   height: 40px;
   margin: 0 0 40px 0;
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
-  transition: box-shadow 0.3s ease; 
+  transition: box-shadow 0.3s ease;
 
   &:hover {
     box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
-    color: #FFEFD5;
+    color: #ffefd5;
     cursor: pointer;
   }
-`
+`;
+
+const MensagemErro = styled.p`
+  color: #ffcc00;
+  font-size: 14px;
+  margin-top: -10px;
+`;
+
+const MensagemSucesso = styled.p`
+  color: #ffcc00;
+  font-size: 14px;
+  margin-top: -10px;
+`;
 
 function PaginaCadastro(props) {
-    const [nome, setNome] = useState('');
-    const [sobrenome, setSobrenome] = useState('');
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-    const [tipoUsuario, setTipoUsuario] = useState('aluno');
-    const [confirmarSenha, setConfirmarSenha] = useState('');
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [tipoUsuario, setTipoUsuario] = useState('aluno');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
+  const [feedbackErro, setFeedbackErro] = useState('');
+  const [feedbackSucesso, setFeedbackSucesso] = useState('');
+  const [senhaSugerida, setSenhaSugerida] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    if (senha !== confirmarSenha) {
+      setFeedbackErro('A senha e a confirmação de senha não coincidem.');
+      return;
+    }
 
-        try {
-            const response = await axios.post('http://localhost:3003/user/signup', 
-            {
-                nome: nome,
-                sobrenome: sobrenome,
-                tipo: tipoUsuario,
-                senha: senha,
-                email: email,
-                senhaConfirmada: confirmarSenha
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json' 
-                },
-            });
-
-            if (response.ok) {
-                setNome('');
-                setSobrenome('');
-                setEmail(''); 
-                setSenha('')
-                setTipoUsuario('')
-                setConfirmarSenha('')
-            }
-        } catch (error) {
-            console.error('Error submitting form:', error);
+    try {
+      const response = await axios.post(
+        'http://localhost:3003/user/signup',
+        {
+          nome: nome,
+          sobrenome: sobrenome,
+          tipo: tipoUsuario,
+          senha: senha,
+          email: email,
+          senhaConfirmada: confirmarSenha,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
         }
-    };
+      );
 
-    return (
+      if (response.status === 200) {
+        setFeedbackSucesso('Cadastro realizado com sucesso!');
+        setNome('');
+        setSobrenome('');
+        setEmail('');
+        setSenha('');
+        setTipoUsuario('aluno');
+        setConfirmarSenha('');
+      }
+    } catch (error) {
+      console.error('Erro ao enviar o formulário:', error);
+      setFeedbackErro('Erro ao enviar o formulário. Tente novamente mais tarde.');
+    }
+  };
+
+  return (
     <FormContainer>
       <Formulario onSubmit={handleSubmit}>
         <CadastroTitulo>Cadastro de Usuário</CadastroTitulo>
 
         <div>
           <Label htmlFor="tipo">Tipo de Usuário:</Label>
-          <Select
-            id="tipo"
-            value={tipoUsuario}
-            onChange={(e) => setTipoUsuario(e.target.value)}
-          >
+          <Select id="tipo" value={tipoUsuario} onChange={(e) => setTipoUsuario(e.target.value)}>
             <option value="aluno">Aluno</option>
             <option value="professor">Professor</option>
             <option value="administrador">Administrador</option>
@@ -166,46 +183,22 @@ function PaginaCadastro(props) {
 
         <div>
           <Label htmlFor="nome">Nome:</Label>
-          <Input
-            type="text"
-            id="nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            required
-          />
+          <Input type="text" id="nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
         </div>
 
         <div>
           <Label htmlFor="sobrenome">Sobrenome:</Label>
-          <Input
-            type="text"
-            id="sobrenome"
-            value={sobrenome}
-            onChange={(e) => setSobrenome(e.target.value)}
-            required
-          />
+          <Input type="text" id="sobrenome" value={sobrenome} onChange={(e) => setSobrenome(e.target.value)} required />
         </div>
 
         <div>
           <Label htmlFor="email">Email:</Label>
-          <Input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          <Input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         </div>
 
         <div>
           <Label htmlFor="senha">Senha:</Label>
-          <Input
-            type="password"
-            id="senha"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            required
-          />
+          <Input type="password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
         </div>
 
         <div>
@@ -217,6 +210,8 @@ function PaginaCadastro(props) {
             onChange={(e) => setConfirmarSenha(e.target.value)}
             required
           />
+          {feedbackErro && <MensagemErro>{feedbackErro}</MensagemErro>}
+          {feedbackSucesso && <MensagemSucesso>{feedbackSucesso}</MensagemSucesso>}
         </div>
 
         <Botao type="submit">Cadastrar</Botao>
@@ -225,4 +220,4 @@ function PaginaCadastro(props) {
   );
 }
 
-export default PaginaCadastro
+export default PaginaCadastro;
