6cd307731c96d7986e38fac2787a10e65a55c657
Victor Hugo Amaral <amaral.vht@gmail.com>
2024-06-15 10:08:17 -0300

    Recuperação da página login.

--- client/explicaaso/src/pages/PaginaLogin.js
+++ client/explicaaso/src/pages/PaginaLogin.js
@@ -1,102 +1,119 @@
-import React, { useState, useContext } from 'react';
+import InputComponent from "../components/Login/InputComponent/InputComponent";
 import styled from 'styled-components';
-import axios from 'axios';
+import axios from "axios"
+import { useContext, useState } from "react";
 import { AuthContext } from '../components/Auth/AuthContext';
 import { useNavigate } from 'react-router-dom';
-import InputComponent from '../components/Login/InputComponent/InputComponent';
 
-const Container = styled.div`
+const ContainerPag = styled.div`
   width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-const LoginContainer = styled.form`
-  background-color: #003466;
-  width: 90%;
-  max-width: 400px; /* Reduzindo a largura máxima do formulário */
-  border-radius: 8px;
-  padding: 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
-`;
+`
 
-const Title = styled.h1`
-  color: #ffcc00;
-  font-size: 28px;
-  margin-bottom: 20px;
-  text-align: center;
-`;
+const LoginContainer = styled.form`
+  display: flex;
+  background-color: #003466;
+  width: ${({$isMobile}) => ($isMobile ? '90%' : '690px')};
+  border-radius: 8px;
+  flex-direction: column;
+  align-items: center;
+  margin: 100px 0 50px 0;
+`
 
-const SubmitButton = styled.button`
-  width: 100%;
+const LoginTitulo = styled.h1`
+  color: #FFCC00;
+  font-size: 36px;
+`
+
+const Submit = styled.button`
+  width: 40%;
   border: none;
   border-radius: 8px;
-  background-color: #ffcc00;
-  font-size: 18px;
+  background-color: #FFCC00;
+  font-size: 28px;
   font-weight: 600;
   color: #003466;
   height: 50px;
-  margin-top: 20px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
+  margin: 0 0 40px 0;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  transition: box-shadow 0.3s ease; 
 
   &:hover {
-    background-color: #ffe6b3;
+    box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
+    color: #FFEFD5;
+    cursor: pointer;
   }
-`;
+`
 
-const ErrorMessage = styled.p`
-  color: #ffcc00;
-  margin-top: 10px;
-  text-align: center;
-`;
 
-const LoginPage = ({ isMobile }) => {
+function PaginaLogin(props) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
-  const [error, setError] = useState('');
   const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
+    // Aqui prevenimos o comportamento padrão do navegador ao acontecimento do evento:
     event.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:3003/user/login', { email, senha }, {
-        headers: {
-          'Content-Type': 'application/json'
+      const response = await axios.post('http://localhost:3003/user/login',
+        {
+          email: email,
+          senha: senha
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
         }
-      });
+      );
 
-      if (response.status === 200) {
-        const { token, tipoUsuario } = response.data;
+      if(response.status === 200) {
         setEmail('');
         setSenha('');
+
+        const token = response.data.token;
+        const tipoUsuario = response.data.tipoUsuario;
+        
+        // Armazena o token no localStorage:
         login(token, tipoUsuario);
-        navigate(`/${tipoUsuario === 'aluno' ? 'pagina-aluno' : tipoUsuario === 'administrador' ? 'pagina-administrador' : 'pagina-professor'}`);
+
+        // Redireciona o usuário com base no tipo de usuário
+        switch (tipoUsuario) {
+          case 'aluno':
+            navigate('/pagina-aluno');
+            break;
+          case 'administrador':
+            navigate('/pagina-administrador');
+            break;
+          case 'professor':
+            navigate('/pagina-professor');
+            break;
+          default:
+            console.error('Tipo de usuário inválido');
+            break;
+        }
       }
-    } catch (error) {
-      setError('Credenciais inválidas. Por favor, tente novamente.');
-      console.error('Erro ao enviar formulário:', error);
+    } catch(error) {
+      console.error('Error submitting form:', error);
     }
-  };
+  }
 
   return (
-    <Container>
-      <LoginContainer onSubmit={handleSubmit}>
-        <Title>Log in</Title>
-        <InputComponent label="Email" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        <InputComponent label="Senha" type="password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
-        <SubmitButton type="submit">Confirmar</SubmitButton>
-        {error && <ErrorMessage>{error}</ErrorMessage>}
+    <ContainerPag>
+      <LoginContainer $isMobile={props.isMobile} onSubmit={handleSubmit}>
+        <LoginTitulo>Log in</LoginTitulo>
+        <InputComponent label='Email' type='email' id='email' value={email} onChange={(event) => setEmail(event.target.value)} required />
+        <InputComponent label='Senha' type='password' id='senha' value={senha} onChange={(event) => setSenha(event.target.value)} required />
+        <Submit type='submit'>
+          Confirmar
+        </Submit>
       </LoginContainer>
-    </Container>
+    </ContainerPag>
   );
-};
+}
 
-export default LoginPage;
+export default PaginaLogin;
