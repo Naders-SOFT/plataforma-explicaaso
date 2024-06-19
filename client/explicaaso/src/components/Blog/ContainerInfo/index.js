d7727566c63497c9227a4d5c8f452661bb95788a
buzzo-cancri <jpedrobuzzo@gmail.com>
2024-06-19 11:26:54 -0300

    Update na lógica do token

--- client/explicaaso/src/components/Blog/ContainerInfo/index.js
+++ client/explicaaso/src/components/Blog/ContainerInfo/index.js
@@ -52,13 +52,20 @@ const BTADICIONAR = styled.button`
 //                      "Oi! Este é um teste de texto do blog. Para ser mais exato, é um teste de como fica a prévia da postagem! Por enquanto, tudo ocorrendo bem. Quero continuar escrevendo até chegar no limite. Lero lero lero, como vai você? Lero lero lero blablabla pipipipopopo ainda nao mas muito legal que foda. Mas e agora, para onde vamos? Ainda temos algumas linhas porque coloquei como limite de 10. Quase em 10, vamos lá time uhuuu não aguento mais isso que saco quando acaba estou quase e agora ja acabou"]
 
 function ContainerInfo({ isMobile, TitulosPosts, TextosPosts }) {
-    const [token, setToken] = useState('');
-    useEffect(() => {
-        setToken(localStorage.getItem('token'));
-    }, []);
-    const editor = token ? jwtDecode(token).tipoUsuario : false;
+    const editor = false;
     const [renderContent, setRenderContent] = useState(<p>Carregando...</p>)
+    
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        editor = token ? jwtDecode(token).tipoUsuario : false;
+        
 
+        window.addEventListener("storage", () => {
+            const token = localStorage.getItem('token');
+            editor = token ? jwtDecode(token).tipoUsuario : false;
+        })
+    }, []);
+    
     useEffect(() => {
         if(blogPosts && blogPosts.length > 0) {
           const parser = new DOMParser();
