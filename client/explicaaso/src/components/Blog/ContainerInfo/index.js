448a05205f112d779180a4155e561b68365c7529
buzzo-cancri <jpedrobuzzo@gmail.com>
2024-06-20 19:14:19 -0300

    Posts mapeados pra página de Blogs

--- client/explicaaso/src/components/Blog/ContainerInfo/index.js
+++ client/explicaaso/src/components/Blog/ContainerInfo/index.js
@@ -1,10 +1,9 @@
 import styled from 'styled-components';
 import BlocoBlog from "../BlocoBlog";
 import placeholder from "../../../images/sobre_nos/placeholder.png"
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { jwtDecode } from 'jwt-decode';
 import { getAttributesFromExtensions } from '@tiptap/react';
-import { useEffect, useState } from 'react';
 
 const ContainerPag = styled.div`
   display: flex;
@@ -51,35 +50,44 @@ const BTADICIONAR = styled.button`
 // const TextosPosts = ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt rem odit quis quaerat. In dolorem praesentium velit ea esse consequuntur cum fugit sequi voluptas ut possimus voluptatibus deserunt nisi eveniet!Lorem ipsum dolor sit amet, consecteturadipisicing elit. Dolorem voluptates vel dolorum autem ex repudiandae iste quasi. Minima explicabo qui necessitatibus porro nihil aliquid deleniti ullam repudiandae dolores corrupti eaque.",
 //                      "Oi! Este é um teste de texto do blog. Para ser mais exato, é um teste de como fica a prévia da postagem! Por enquanto, tudo ocorrendo bem. Quero continuar escrevendo até chegar no limite. Lero lero lero, como vai você? Lero lero lero blablabla pipipipopopo ainda nao mas muito legal que foda. Mas e agora, para onde vamos? Ainda temos algumas linhas porque coloquei como limite de 10. Quase em 10, vamos lá time uhuuu não aguento mais isso que saco quando acaba estou quase e agora ja acabou"]
 
-function ContainerInfo({ isMobile, TitulosPosts, TextosPosts }) {
-    const editor = false;
+function ContainerInfo({ isMobile, blogPosts }) {
+    const [editor, setEditor] = useState('');
     const [renderContent, setRenderContent] = useState(<p>Carregando...</p>)
     
     useEffect(() => {
         const token = localStorage.getItem('token');
-        editor = token ? jwtDecode(token).tipoUsuario : false;
+        setEditor(token ? jwtDecode(token).tipoUsuario : '');
         
 
         window.addEventListener("storage", () => {
             const token = localStorage.getItem('token');
-            editor = token ? jwtDecode(token).tipoUsuario : false;
+            setEditor(token ? jwtDecode(token).tipoUsuario : '');
         })
     }, [editor]);
     
     useEffect(() => {
         if(blogPosts && blogPosts.length > 0) {
-          const parser = new DOMParser();
-          const texto = parser.parseFromString(blogPosts[0].texto, "text/html");
+          console.log(blogPosts);
+          // const parser = new DOMParser();
+          // const texto = parser.parseFromString(blogPosts[0].texto, "text/html");
 
 
-          setRenderContent(<BlocoBlog
-          isMobile={isMobile}
-          imgSrc={placeholder}
-          imgAlt="placeholder"
-          titulopost = {blogPosts[0].titulo}
-          textopost = {texto.body.textContent}
-          editor={editor}
-        />);
+          setRenderContent(
+            blogPosts.map( ( item, index ) => (
+              <BlocoBlog
+              key={index}
+              isMobile={isMobile}
+              imgSrc={placeholder}
+              imgAlt="placeholder"
+              editor={editor}
+              titulopost={item.titulo}
+              textopost={item.texto}
+              autorpost={item.autor}
+              datapost={item.data}
+              />)
+            )
+          );
+          console.log(renderContent.type);
         }
 
         else{
