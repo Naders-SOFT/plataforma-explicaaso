24f3b908a72438727a6b607b72f77bf39af77450
IsaacHaRR <isaac15rosendo@gmail.com>
2024-06-21 17:02:15 -0300

    Styled Botões

--- client/explicaaso/src/components/Administrador/SideBar/index.js
+++ client/explicaaso/src/components/Administrador/SideBar/index.js
@@ -103,12 +103,11 @@ function SideBar(props) {
                 props.handleSelectedUsuarios(usuarios);
             }
 
-          } catch (error) {
+        } catch (error) {
             console.error(error);
-          }
+        }
 
-        
-
+    
     };
 
     
