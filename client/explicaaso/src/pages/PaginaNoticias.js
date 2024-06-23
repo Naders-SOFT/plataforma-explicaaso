be606f8c32664f9ada0aa4b2ff3e73cbb214e035
RafaelCGConrado <106274045+RafaelCGConrado@users.noreply.github.com>
2024-06-23 17:43:30 -0300

    trabalhando na integracao de noticias

--- client/explicaaso/src/pages/PaginaNoticias.js
+++ client/explicaaso/src/pages/PaginaNoticias.js
@@ -1,5 +1,7 @@
 import ContainerInfo from "../components/Noticias/ContainerInfo";
 import styled from 'styled-components';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const ContainerPag = styled.div`
     width: 100%;
@@ -8,6 +10,18 @@ const ContainerPag = styled.div`
 
 
 function PaginaNoticias(props) {
+    const [noticiaPosts, setNoticiaPosts] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:3003/noticia/list')
+        .then( response => {
+            setNoticiaPosts(response.data);
+        })
+        .catch( error => {
+            console.error('Error fetching data', error);
+        })
+    }, []);
+    
+    
     return (
         <ContainerPag>
             <ContainerInfo isMobile={props.isMobile}/>
---
+++ server/routes/noticia.routes.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { createnoticiaPost, 
+    listnoticiaPosts,
+    listblogPostById,
+    updatenoticiaPost,
+    deletenoticiaPost } from '../controllers/noticiaPost.controller.js';
+
+const router = Router();
+
+router.post('/post', createnoticiaPost);
+
+router.get('/list', listnoticiaPosts);
+
+router.get('/list/:idnoticiaPost', listnoticiaPostById);
+
+router.patch('/update/:idnoticiaPost', updatenoticiaPost);
+
+router.delete('/delete/:idnoticiaPost', deletenoticiaPost);
+
+export default router;
