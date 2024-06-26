3a6ff06cd52e1208047c5699ecfa0bbc66e7f24f
João P. Hamata <128185132+joaophamata@users.noreply.github.com>
2024-06-26 09:40:35 -0300

    Correção de responsividade

--- client/explicaaso/src/components/Disciplina/Frentes/index.js
+++ client/explicaaso/src/components/Disciplina/Frentes/index.js
@@ -15,6 +15,7 @@ const ContainerFrentes = styled.div`
   height: 100%;
   background-color: #f0f0f5;
   flex-direction: ${({$isMobile}) => ($isMobile ? 'column' : '')};
+  overflow-x: hidden; 
 `;
 
 const ContentArea = styled.div`
