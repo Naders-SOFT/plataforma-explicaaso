6eb9adbca8737c65e88ad904377673d71703efaf
João P. Hamata <128185132+joaophamata@users.noreply.github.com>
2024-06-27 00:47:18 -0300

    Correção de borda

--- client/explicaaso/src/pages/PaginaCadastro.js
+++ client/explicaaso/src/pages/PaginaCadastro.js
@@ -9,8 +9,8 @@ const FormContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-height: 100vh;
   padding: 0;
+  margin: 5%;
 `;
 
 const CadastroTitulo = styled.h1`
